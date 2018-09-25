034ac3d55063ccfdd29b31bd1d60ea50e7607afc
Spatocode <impulseofficial069@gmail.com>
2018-09-25 12:43:40 +0100

    title

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,22 +6,22 @@ const Menu = () =>
         <nav>
             <ul>
                 <li>
-                    <NavLink to="/"><span className="icon"><i className="fa fa-home"></i></span><span className="name">Home</span></NavLink>
+                    <NavLink to="/" title="Home"><span className="icon"><i className="fa fa-home"></i></span><span className="name">Home</span></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/science"><span className="icon"><i className="fa fa-flask"></i></span><span className="name">Science</span></NavLink>
+                    <NavLink to="/science" title="Science"><span className="icon"><i className="fa fa-flask"></i></span><span className="name">Science</span></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/sports"><span className="icon"><i className="fa fa-futbol-o"></i></span><span className="name">Sports</span></NavLink>
+                    <NavLink to="/sports" title="Sports"><span className="icon"><i className="fa fa-futbol-o"></i></span><span className="name">Sports</span></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/technology"><span className="icon"><i className="fa fa-rocket"></i></span><span className="name">Technology</span></NavLink>
+                    <NavLink to="/technology" title="Technology"><span className="icon"><i className="fa fa-rocket"></i></span><span className="name">Technology</span></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/politics"><span className="icon"><i className="fa fa-bookmark"></i></span><span className="name">Politics</span></NavLink>
+                    <NavLink to="/politics" title="Politics"><span className="icon"><i className="fa fa-bookmark"></i></span><span className="name">Politics</span></NavLink>
                 </li>
                 <li>
-                    <NavLink to="/business" ><span className="icon"><i className="fa fa-briefcase"></i></span><span className="name">Business</span></NavLink>
+                    <NavLink to="/business" title="Business"><span className="icon"><i className="fa fa-briefcase"></i></span><span className="name">Business</span></NavLink>
                 </li>
             </ul>
         </nav>
