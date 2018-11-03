801a4321b74d0dbf1dc2c3dc39d18ebbe72b7e47
Spatocode <impulseofficial069@gmail.com>
2018-11-03 13:04:58 +0100

    Made reducers accessible

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,17 +18,26 @@ export function navReducer(state = {fullNav: false}, action){
     }
 }
 
-export function news(state = initialState, action){
+export function selectNews(state={category: "general"}, action) {
+    switch(action.type){
+        case C.SELECT_NEWS:
+            return Object.assign({}, state, {
+                category: action.category
+            })
+        default:
+            return state
+    }
+}
+
+export function news(state = initialState, action) {
     switch(action.type){
         case C.REQUEST_NEWS:
             return Object.assign({}, state, {
                 isFetching: true,
-                didInvalidate: false
             })
         case C.RECEIVE_NEWS:
             return Object.assign({}, state, {
                 isFetching: false,
-                didInvalidate: false,
                 items: action.news
             })
         default:
